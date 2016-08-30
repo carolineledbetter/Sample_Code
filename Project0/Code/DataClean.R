6942aee91ec572ced55fd3d882b0dc18b584afa8
Caroline Ledbetter <caroline.ledbetter@ucdenver.edu>
2016-08-30 13:30:29 -0600

    Add data import/clean code
    Created code for reading in data set into R, creating varaibles for difference between base and 1 year and saving as new R data set.

---
+++ Project0/Code/DataClean.R
@@ -0,0 +1,19 @@
+# Load data from CSV into R. 
+Project0_dental_data <- read.csv(
+  "~/Library/Mobile Documents/com~apple~CloudDocs/BIOS 6623/Datasets/Project0_dental_data.csv",
+  row.names=NULL)
+#Convert intergers for categorical variables to factors for easier analysis.
+Project0_dental_data$sex <- factor(Project0_dental_data$sex)
+levels(Project0_dental_data$sex) <- c("male","female")
+Project0_dental_data$race <- factor(Project0_dental_data$race)
+levels(Project0_dental_data$race) <- c("Native American", "African American",
+                                       "Asian", "White")
+Project0_dental_data$smoker <- factor(Project0_dental_data$smoker)
+levels(Project0_dental_data$smoker) <- c("No", "Yes")
+#add varaibles for difference between baseline and 1 year measurements
+Project0_dental_data$attachdiff <- Project0_dental_data$attach1year -
+  Project0_dental_data$attachbase
+Project0_dental_data$pddiff <- Project0_dental_data$pd1year - 
+  Project0_dental_data$pdbase
+save(Project0_dental_data, 
+     file = '/Users/CarolineL/Repositories/Data/Project0Data/Project0cleandata.rda')
