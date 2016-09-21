1d806b16ba236c8c732b4416c632468d964cb266
ledbettc <Caroline.Ledbetter@ucdenver.edu>
2016-09-21 11:48:36 -0600

    working on descriptives.

--- Project1/Code/Descriptives.R
+++ Project1/Code/Descriptives.R
@@ -1,11 +1,17 @@
 #attach data set so variables can be accessed directly
 attach(camp_teach)
 
-table(TX)#look at treatment groups
-table(TG)
+#get demographics by id
+ids <- unique(id) 
 
+#create empty data frame with structure of first 6 columns
+#only treatment, age at enrollment, gender, and ethnicity are constant for all 
+#observations of a given id.
+desc <- camp_teach[0,1:6]
+invisible(lapply(ids, function(i) {
+  desc <<- rbind.data.frame(desc,camp_teach[camp_teach$id == i,1:6][1,])
+}))
 
 #look at demographics
 source("/Users/Caroline/Repositories/Table1/Table1.r")
-Table1(c("age_rz","GENDER", "ETHNIC","wbc", "agehome", "anypet", "woodstove", "dehumid",
-         "parent_smokes", "any_smokes"),2, data = camp_teach)
+Table1(c("age_rz","GENDER", "ETHNIC"),1, data = desc)
