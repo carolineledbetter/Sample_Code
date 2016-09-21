1b4e39fde26dad02b40ad41dd3e9e879b288270b
ledbettc <caroline.ledbetter@ucdenver.edu>
2016-09-21 15:45:04 -0600

    Finsished descriptive analysis and created report.

--- Project1/Code/Descriptives.R
+++ Project1/Code/Descriptives.R
@@ -1,3 +1,6 @@
+#load data set
+load(file = '/Users/Caroline/Repositories/Data/Project1Data/camp_teach.rda')
+
 #attach data set so variables can be accessed directly
 attach(camp_teach)
 
@@ -9,9 +12,24 @@ ids <- unique(id)
 #observations of a given id.
 desc <- camp_teach[0,1:6]
 invisible(lapply(ids, function(i) {
-  desc <<- rbind.data.frame(desc,camp_teach[camp_teach$id == i,1:6][1,])
-}))
+  desc <<- rbind.data.frame(desc,unique(camp_teach[camp_teach$id == i, 1:6])
+)
+}))#obs in desc should match length of ids- it does!
 
+
+#create a dataset with the number of observations for each id
+ids_obs <- data.frame("ids" = ids, "obs" = unlist(lapply(ids, function(i) {length(camp_teach[camp_teach$id == i,3])})))
+#get range and mean # of observation
+range(ids_obs$obs); mean(ids_obs$obs)
+
+#change names for demographics for clarity/prettiness and expand factors
+names(desc) <- c(names(desc)[1:3],"Age at Enrollment", "Gender", "Ethnicity")
+levels(desc$Gender) <- c("Female", "Male")
+levels(desc$Ethnicity) <- c("Black", "Hispanic", "Other", "Non-Hispanic White")
 #look at demographics
 source("/Users/Caroline/Repositories/Table1/Table1.r")
-Table1(c("age_rz","GENDER", "ETHNIC"),1, data = desc)
+Table_1 <- Table1(c(4:6),1, data = desc)
+
+#save workspace for use in .rmd
+save.image("~/Repositories/bios6623-ledbettc/Project1/Descriptives.RData")
+
---
+++ Project1/Reports/Readme.md
@@ -0,0 +1,3 @@
+##This folder contains the reports for Project1  
+.PDFs are created using RMarkdown(.rmd)  
+Descriptives.rmd/.pdf is a report on the characterstics of the data and the study participants.  
